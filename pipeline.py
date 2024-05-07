e086f876de19e673bd4ad715f6d8c2fda5f85cb2
Aidan Teo Melvin <c4700000@tron48.umiacs.umd.edu>
2024-05-06 20:52:10 -0400

    added checkpoint code

---
+++ pipeline.py
@@ -0,0 +1,43 @@
+from transformers import pipeline
+
+print('INFO: Finished Imports')
+
+with open('./filing_text.txt', 'r') as fh:
+    filing_full_text = '\n'.join(fh.readlines())
+
+print('INFO: Read in SEC 8-K filing from filing_text.txt')
+
+def clean_text(text):
+    cleaned_text = text.encode().decode('unicode_escape')
+    cleaned_text = cleaned_text.replace('\\n', '\n')
+    return cleaned_text
+
+with open('./filing_text.txt', 'r') as fh:
+    input_text = fh.read()
+
+cleaned_text = clean_text(input_text)
+
+paragraphs = []
+for paragraph in cleaned_text.split('\n\n'):
+    paragraphs.append(paragraph.strip())
+
+
+pipe = pipeline("text-classification", model="ProsusAI/finbert")
+print('INFO: Created text classification pipeline')
+
+# gather paragraphs with either very positive or very negative
+# sentiment for summarization
+paragraphs_to_summarize = []
+for paragraph in paragraphs:
+    scores = pipe(paragraph)
+    if scores[0]['label'] != 'neutral' and scores[0]['score'] > 0.5:
+        paragraphs_to_summarize.append((scores[0]['label'], paragraph))
+
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+print('INFO: Created text summarization pipeline')
+
+for paragraph in paragraphs_to_summarize:
+    print(f'Summary of paragraph with very {paragraph[0]} sentiment:')
+    print(summarizer(paragraph[1], max_length=130, min_length=30, do_sample=False))
+
+
