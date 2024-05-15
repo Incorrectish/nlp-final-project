07630e61148e93367fe41edf92c813dbf8a4a62f
Ishan <ishan@wireless-10-105-157-157.umd.edu>
2024-05-15 13:03:49 -0400

    added sentiment

--- final_project.py
+++ final_project.py
@@ -1,6 +1,6 @@
 import fetch_data
 import summarize 
-import label
+import sentiment
 import topic_segmentation 
 import sys
 
@@ -10,7 +10,7 @@ def main():
         file_text = html_file.read()
         filtered_text = fetch_data.filter_html(file_text)
         summary = summarize.summarize(filtered_text)
-        sentiment = label.sentiment(filtered_text)
+        paragraph_sentiment = sentiment.sentiment(filtered_text)
         # output is in segments.txt
         topic_segmentation.topic_segmentation(filtered_text)
 
